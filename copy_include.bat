f090bc415427913eb58a1709e665c26227ed350e
Nikita Cheblokov <ninja11103@gmail.com>
2020-07-13 19:47:09 -0700

    Update build scripts and gitignore

--- copy_include.bat
+++ copy_include.bat
@@ -1,4 +1,5 @@
 cd ..
 ROBOCOPY . .\build\%1\%2\include\ *.h /S /XD build
+get_stun.bat %1 %2
 set errorlevel=0
 if %errorlevel% lss 16 exit 0
---
+++ get_stun.bat
@@ -0,0 +1,5 @@
+wget http://www.stunprotocol.org/stunserver_win64_1_2_16.zip -P .\build\%1\%2\bin\Stun
+tar -xf .\build\%1\%2\bin\Stun\stunserver_win64_1_2_16.zip -C .\build\%1\%2\bin\Stun
+xcopy /s .\build\%1\%2\bin\Stun\release .\build\%1\%2\bin\Stun
+rmdir .\build\%1\%2\bin\Stun\release /s /q
+del /f .\build\%1\%2\bin\Stun\stunserver_win64_1_2_16.zip
